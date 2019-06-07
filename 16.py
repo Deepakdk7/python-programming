5932f2fa42f259f3323aae6f112597b89fee4175
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-07 09:49:14 +0530

    Create 16.py

---
+++ 16.py
@@ -0,0 +1,11 @@
+ax=input()
+ax=ax.split()
+ax=list(map(int,ax))
+a1=ax[0]
+a2=ax[1]
+for n in range(a1+1,a2):
+    for i in range(2,n):
+        if n%i==0:
+            break
+    else:
+        print(i,"",end="")
