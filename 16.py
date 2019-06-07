34f123c2172e8332efdb9a35bbfc7977caf21583
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-07 09:55:05 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -5,7 +5,7 @@ a1=ax[0]
 a2=ax[1]
 for n in range(a1+1,a2):
     for i in range(2,n):
-        if n%i==0:
+        if(n%i==0):
             break
     else:
-        print(i,"",end="")
+        print(n,"",end="")
